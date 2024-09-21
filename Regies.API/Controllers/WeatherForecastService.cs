3170bd7ad1e185d294d6a29302e90c065b77ceda
HugoLevk <hugo.levk@gmail.com>
2024-09-21 16:26:48 +0200

    Exercice 1

--- Regies.API/Controllers/WeatherForecastService.cs
+++ Regies.API/Controllers/WeatherForecastService.cs
@@ -1,8 +1,9 @@
 ﻿namespace Regies.API.Controllers;
 
+
 public interface IWeatherForecastService
 {
-    IEnumerable<WeatherForecast> Get(int max, int Number);
+    IEnumerable<WeatherForecast> Get(int min, int max, int Number);
 }
 
 public class WeatherForecastService : IWeatherForecastService
@@ -11,12 +12,12 @@ public class WeatherForecastService : IWeatherForecastService
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     ];
 
-    public IEnumerable<WeatherForecast> Get(int max, int Number)
+    public IEnumerable<WeatherForecast> Get(int min, int max, int Number)
     {
         return Enumerable.Range(1, Number).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-55, max),
+            TemperatureC = Random.Shared.Next(min, max),
             Summary = s_Summaries[Random.Shared.Next(s_Summaries.Length)]
         })
         .ToArray();
