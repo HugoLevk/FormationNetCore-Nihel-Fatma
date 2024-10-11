b569882f4f6d363bb28b45dbf98a4b8652daa908
HugoLevk <hugo.leveque@web-atrio.com>
2024-10-11 14:25:35 +0200

    Dto Conversion

--- Application/Regies/DTOs/RegieDTO.cs
+++ Application/Regies/DTOs/RegieDTO.cs
@@ -11,4 +11,32 @@ public class RegieDto
     public Adresse Adresse { get; set; } = default!;
     public List<BienImmobilier> lesBiensDeLaRegie { get; set; } = [];
 
+    public static RegieDto? FromRegie(Regie? regie)
+    {
+        if (regie == null) return null;
+        return new RegieDto
+        {
+            Id = regie.Id,
+            Nom = regie.Nom,
+            Description = regie.Description,
+            estActive = regie.estActive,
+            Adresse = regie.Adresse,
+            lesBiensDeLaRegie = regie.lesBiensDeLaRegie
+        };
+    }
+
+    public static Regie? FromDto(RegieDto? regieDto)
+    {
+        if (regieDto == null) return null;
+        return new Regie
+        {
+            Id = regieDto.Id,
+            Nom = regieDto.Nom,
+            Description = regieDto.Description,
+            estActive = regieDto.estActive,
+            Adresse = regieDto.Adresse,
+            lesBiensDeLaRegie = regieDto.lesBiensDeLaRegie
+        };
+    }
+
 }
--- Application/Regies/RegieService.cs
+++ Application/Regies/RegieService.cs
@@ -6,37 +6,16 @@ namespace Regies.Application.Regies;
 
 public class RegieService(IRegieRepository regieRepository) : IRegieService
 {
-    public async Task<IEnumerable<RegieDto>> GetAllRegies()
+    public async Task<IEnumerable<RegieDto?>> GetAllRegies()
     {
         var regies = await regieRepository.GetAllAsync();
-        return regies.Select(r => new RegieDto
-        {
-            Id = r.Id,
-            Nom = r.Nom,
-            Description = r.Description,
-            estActive = r.estActive,
-            Adresse = r.Adresse,
-            lesBiensDeLaRegie = r.lesBiensDeLaRegie
-        });
+        return regies.Select(r => RegieDto.FromRegie(r));
     }
 
     public async Task<RegieDto?> GetRegieById(int id)
     {
         var regie = await regieRepository.GetByIdAsync(id);
-        RegieDto? regieDto = null;
-
-        if (regie != null)
-        {
-            regieDto = new RegieDto
-            {
-                Id = regie.Id,
-                Nom = regie.Nom,
-                Description = regie.Description,
-                estActive = regie.estActive,
-                Adresse = regie.Adresse,
-                lesBiensDeLaRegie = regie.lesBiensDeLaRegie
-            };
-        }
+        RegieDto? regieDto = RegieDto.FromRegie(regie);
         return regieDto;
     }
 
