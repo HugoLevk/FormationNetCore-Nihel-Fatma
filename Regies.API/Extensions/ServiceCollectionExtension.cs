a7e140874737ab4d8604f95c3dcec48ac3cee845
HugoLevk <hugo.leveque@web-atrio.com>
2024-10-16 15:13:10 +0200

    Presentation

---
+++ Regies.API/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.OpenApi.Models;
+using Regies.API.Middlewares;
+using Regies.API.SchemaFilters;
+using Serilog;
+using System.Reflection;
+
+namespace Regies.API.Extensions;
+
+public static class ServiceCollectionExtension
+{
+    public static void AddPresentation(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new()
+            {
+                Title = "Regies.API",
+                Version = "v1",
+                Description = "API pour la gestion des régies.",
+                Contact = new()
+                {
+                    Name = "Regies",
+                    Email = "hugo.leveque@web-atrio.com",
+                },
+
+            });
+
+            c.SchemaFilter<EnumSchemaFilter>();
+
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+            c.IncludeXmlComments(xmlPath);
+
+            var applicationXmlFile = $"../../../../Application/bin/Debug/net8.0/Regies.Application.xml";
+            var applicationXmlPath = Path.Combine(AppContext.BaseDirectory, applicationXmlFile);
+
+            c.IncludeXmlComments(applicationXmlPath);
+
+            c.AddSecurityDefinition("bearerAuth", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.Http,
+                Scheme = "Bearer"
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference{ Type = ReferenceType.SecurityScheme, Id = "bearerAuth"}
+            },
+            []
+        }
+    });
+
+        });
+
+        builder.Host.UseSerilog((context, configuration) =>
+        {
+            configuration.ReadFrom.Configuration(context.Configuration);
+        });
+
+        builder.Services.AddScoped<ErrorHandlingMiddleware>();
+        builder.Services.AddScoped<TimeLoggingMiddleware>();
+
+        builder.Services.AddControllers().ConfigureApiBehaviorOptions(x => { x.SuppressMapClientErrors = true; });
+    }
+}
--- Regies.API/Program.cs
+++ Regies.API/Program.cs
@@ -8,68 +8,15 @@ using Regies.API.SchemaFilters;
 using Regies.API.Middlewares;
 using Regies.Domain.Model;
 using Microsoft.OpenApi.Models;
+using Regies.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddSwaggerGen( c =>
-{
-    c.SwaggerDoc("v1", new()
-    {
-        Title = "Regies.API",
-        Version = "v1",
-        Description = "API pour la gestion des régies.",
-        Contact = new()
-        {
-            Name = "Regies",
-            Email = "hugo.leveque@web-atrio.com",
-        },
-        
-    });
-
-    c.SchemaFilter<EnumSchemaFilter>();
-
-    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-
-    c.IncludeXmlComments(xmlPath);
-
-    var applicationXmlFile = $"../../../../Application/bin/Debug/net8.0/Regies.Application.xml";
-    var applicationXmlPath = Path.Combine(AppContext.BaseDirectory, applicationXmlFile);
-
-    c.IncludeXmlComments(applicationXmlPath);
-
-    c.AddSecurityDefinition("bearerAuth", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-    {
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer"
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference{ Type = ReferenceType.SecurityScheme, Id = "bearerAuth"}
-            },
-            []
-        }
-    });
-
-});
-
-builder.Services.AddScoped<ErrorHandlingMiddleware>();
-builder.Services.AddScoped<TimeLoggingMiddleware>();
-
-builder.Services.AddControllers().ConfigureApiBehaviorOptions(x => { x.SuppressMapClientErrors = true; }); 
-
+// Add services to the container
+builder.AddPresentation();
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 
-builder.Host.UseSerilog((context, configuration) =>
-{
-    configuration.ReadFrom.Configuration(context.Configuration);
-});
 
 
 var app = builder.Build();
